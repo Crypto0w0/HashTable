517e583be7d0d148fdb19456f3ff5a6bdc787d32
Alex Untilov <93001397+Crypto0w0@users.noreply.github.com>
2023-10-22 19:03:29 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,67 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        
+    }
+
+    class HTable
+    {
+        List<KeyValuePair<int, string>> list;
+
+        public HTable()
+        {
+            list = new List<KeyValuePair<int, string>>(0);
+        }
+
+        public void Add(KeyValuePair<int, string> el)
+        {
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (list[i].Key == el.Key)
+                {
+                    Console.WriteLine("Key already exists");
+                    return;
+                }
+            }
+            list.Add(el);
+        }
+
+        public void Remove(int key)
+        {
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (list[i].Key == key)
+                {
+                    Move(i);
+                    return;
+                }
+            }
+            Console.WriteLine("Key do not exist");
+        }
+
+        void Move(int index)
+        {
+            int i;
+            for (i = index; i < list.Count - 1; i++)
+            {
+                list[i] = list[i + 1];
+            }
+            list.RemoveAt(i);
+        }
+
+        public string Search(int key)
+        {
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (list[i].Key == key) return list[i].Value;
+            }
+            return null;
+        }
+
+        public int GetLength()
+        {
+            return list.Count;
+        }
+    }
+}
